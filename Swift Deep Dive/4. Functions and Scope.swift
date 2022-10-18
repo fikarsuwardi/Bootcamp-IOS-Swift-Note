ead7cae16ecafe6165ab42befbad0c89d24a008e
Fikar Suwardi <88697103+fikarsuwardi@users.noreply.github.com>
2022-10-18 17:27:01 +0700

    Create 4. Functions and Scope.swift

---
+++ Swift Deep Dive/4. Functions and Scope.swift
@@ -0,0 +1,13 @@
+func greeting() {
+  print("Hello")
+  var nameAmma = "Amma"
+}
+
+func greeting2() {
+  print("Hello 2")
+  // cant use variable name here
+  print(nameAmma) // will be error
+}
+
+greeting()
+greeting2()
