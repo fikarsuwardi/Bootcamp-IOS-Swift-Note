247157f289035ace607116e5a5792569e2b75ab9
Fikar Suwardi <88697103+fikarsuwardi@users.noreply.github.com>
2022-10-18 09:35:16 +0700

    Update MyStructure.swift

--- MyStructure.swift
+++ MyStructure.swift
@@ -3,9 +3,20 @@ import Foundation
 struct MyStructure {
   let instanceProperty = "ABC"
   static let typeProperty = "123"
+  
+  func instanceMethod() {
+  
+  }
+  
+  func typeMethod() {
+  
+  }
 }
 
 let myStructure = MyStructure()
 
 print(myStructure.instanceProperty)
 print(MyStructure.typeProperty)
+
+myStructure.instanceMethod()
+MyStructure.typeMethod()
