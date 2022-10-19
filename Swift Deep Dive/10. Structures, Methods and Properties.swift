9f55cf9f5c45f32063e079348178d7cf51ac1ecd
Fikar Suwardi <88697103+fikarsuwardi@users.noreply.github.com>
2022-10-19 09:57:23 +0700

    Update 10. Structures, Methods and Properties.swift

--- Swift Deep Dive/10. Structures, Methods and Properties.swift
+++ Swift Deep Dive/10. Structures, Methods and Properties.swift
@@ -56,3 +56,39 @@ struct City {
 
 var ammaCity = City(name: "Ammaland", citizen: ["Amma", "Fikar"], resources: ["Roti": 50, "Selai": 10])
 print(ammaCity) // City(name: "Ammaland", citizen: ["Amma", "Fikar"], resources: ["Roti": 50, "Selai": 10])
+
+// Challenge
+// Define the User struct here
+struct User {
+  let name: String
+  var email: String?
+  var followers: Int
+  var isActive: Bool
+  
+  func logStatus() {
+    if isActive == true {
+      print("\(name) is working hard")
+    } else {
+      print("\(name) has left earth")
+    }
+  }
+  
+  init(name: String, email: String, followers: Int, isActive: Bool) {
+    self.name = name
+    self.email = email
+    self.followers = followers
+    self.isActive = isActive
+  }
+}
+
+// Initialise a User struct here
+
+// Diagnostic code - do not change this code
+print("\nDiagnostic code (i.e., Challenge Hint):")
+var musk = User(name: "Elon", email: "elon@tesla.com", followers: 2001, isActive: true)
+musk.logStatus()
+print("Contacting \(musk.name) on \(musk.email!) ...")
+print("\(musk.name) has \(musk.followers) followers")
+// sometime later
+musk.isActive = false
+musk.logStatus()
