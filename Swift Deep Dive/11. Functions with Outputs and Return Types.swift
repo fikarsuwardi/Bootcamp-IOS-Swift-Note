cd56fce4d5d2776ae7931ae8baf17349402382b5
Fikar Suwardi <88697103+fikarsuwardi@users.noreply.github.com>
2022-10-19 10:03:00 +0700

    Create 11. Functions with Outputs and Return Types.swift

---
+++ Swift Deep Dive/11. Functions with Outputs and Return Types.swift
@@ -0,0 +1,8 @@
+//11. Functions with Outputs and Return Types
+func greeting3(name: String) -> String {
+  print("Haii \(name)")
+  return "Hello \(name)"
+}
+
+greeting3(name: "Sijia Afriani") // Haii Sijia Afriani
+print(greeting3(name: "Sijia Afriani")) // 2 line
