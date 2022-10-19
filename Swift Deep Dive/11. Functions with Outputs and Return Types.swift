d78c10d55fe40babd93ae7730b5ed926c1732e4c
Fikar Suwardi <88697103+fikarsuwardi@users.noreply.github.com>
2022-10-19 10:07:27 +0700

    Update 11. Functions with Outputs and Return Types.swift

--- Swift Deep Dive/11. Functions with Outputs and Return Types.swift
+++ Swift Deep Dive/11. Functions with Outputs and Return Types.swift
@@ -6,3 +6,15 @@ func greeting3(name: String) -> String {
 
 greeting3(name: "Sijia Afriani") // Haii Sijia Afriani
 print(greeting3(name: "Sijia Afriani")) // 2 line
+
+// Challenge
+//Create your function here:
+func isOdd(n: Int) -> Bool {
+    if n % 2 == 0 {
+        return false
+    } else {
+        return true
+    }
+}
+
+print(isOdd(n: 9))
