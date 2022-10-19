82708fbd6062485188481bde870bd32ed392a040
Fikar Suwardi <88697103+fikarsuwardi@users.noreply.github.com>
2022-10-19 08:33:33 +0700

    Update 9. Defining and Unwrapping Optionals.swift

--- Swift Deep Dive/9. Defining and Unwrapping Optionals.swift
+++ Swift Deep Dive/9. Defining and Unwrapping Optionals.swift
@@ -6,3 +6,12 @@ var player1Username: String?
 player1Username = "SijiaAfriani"
 print(player1Username) // Optional("SijiaAfriani")
 print(player1Username!) // Sijia Afriani
+
+var player2Username: String?
+player2Username = "Halida Husain"
+player2Username = nil
+print(player2Username) // nil
+
+if player2Username != nil { // it will skip the line
+  print(player2Username!)
+}
